8a7c5e0e7ee37172a2c90acdc875647d977aaf3a
EricWendel <eawendel19@gmail.com>
2021-12-15 11:09:38 -0600

    Dynamic route for recipes

--- pages/recipe/[recipeId].js
+++ pages/recipe/[recipeId].js
@@ -1,7 +1,41 @@
-export default function Home() {
+export default function Home(props) {
   return (
     <div>
-      <h1>Sample Recipe</h1>
+      <h1>{props.recipeId}</h1>
     </div>
   );
+}
+
+export async function getStaticPaths() {
+  return {
+    fallback: true, // true: allow any path, false: 404 error for other paths
+    paths: [
+      // these are pre loaded, faster than other paths
+      {
+        params: {
+          recipeId: "ChickenSandwich",
+        },
+      },
+      {
+        params: {
+          recipeId: "ApplePie",
+        },
+      },
+      {
+        params: {
+          recipeId: "Muffin",
+        },
+      },
+    ],
+  };
+}
+
+export async function getStaticProps(context) {
+  const recipeId = context.params.recipeId;
+
+  return {
+    props: {
+      recipeId: recipeId,
+    },
+  };
 }
