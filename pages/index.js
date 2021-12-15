d1c28388e375c59d286bfbd64a11957bbd292cc1
EricWendel <eawendel19@gmail.com>
2021-12-15 03:37:10 -0600

    Home page links

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,24 @@
+import Link from "next/link";
+import { Fragment } from "react/cjs/react.production.min";
+import styles from "../styles/Home.module.css";
 
-export default function Home(){
-    return <h1>Recipe Home</h1>
+export default function Home() {
+  return (
+    <Fragment>
+      <h1 className={styles.title}>Recipe Home</h1>
+      <div className={styles.main}>
+        <ul>
+          <p className={styles.link}>
+            <Link href="/recipe/ChickenSandwich">Chicken Sandwich</Link>
+          </p>
+          <p className={styles.link}>
+            <Link href="/recipe/ApplePie">Apple Pie</Link>
+          </p>
+          <p className={styles.link}>
+            <Link href="/recipe/Muffins">Muffins</Link>
+          </p>
+        </ul>
+      </div>
+    </Fragment>
+  );
 }
---
+++ pages/recipe/[recipeId].js
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div>
+      <h1>Sample Recipe</h1>
+    </div>
+  );
+}
