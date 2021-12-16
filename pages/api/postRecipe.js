6828232c937f1cf8f76ea7fdd37480de67e98465
EricWendel <eawendel19@gmail.com>
2021-12-16 00:26:09 -0600

    Mongodb setup with Atlas

--- pages/api/postRecipe.js
+++ pages/api/postRecipe.js
@@ -1,8 +1,30 @@
-export default function handler(req, res) {
+import { MongoClient } from "mongodb";
+
+export default async function handler(req, res) {
   if (req.method === "POST") {
     const { title, user, image, desc } = req.body;
-    // Need to setup database before this is useful
-    res.status(200).json({
+    const client = await MongoClient.connect(
+      "mongodb+srv://" +
+        process.env.DBUSER +
+        ":" +
+        process.env.DBPASS +
+        "@" +
+        process.env.DBNAME +
+        ".xrcz2.mongodb.net/recipes?retryWrites=true&w=majority"
+    );
+    const db = client.db();
+    const collection = db.collection("recipes");
+    const result = await collection.insertOne({
+      title: title,
+      recipeName: title.replace(/ /g, ""),
+      user: user,
+      image: image,
+      desc: desc,
+    });
+    console.log(result);
+    client.close();
+
+    res.status(201).json({
       title: title,
       recipeName: title.replace(/ /g, ""),
       user: user,
