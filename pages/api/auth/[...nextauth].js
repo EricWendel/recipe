2fd6924ff3959879f45a1e38a2a5499ad70da6d1
EricWendel <eawendel19@gmail.com>
2021-12-29 23:02:57 -0600

    Auth using prisma

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,7 +1,9 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
-import { MongoDBAdapter } from "@next-auth/mongodb-adapter";
-import clientPromise from "./lib/mongodb.js";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
 
 export default NextAuth({
   providers: [
@@ -18,5 +20,5 @@ export default NextAuth({
     }),
   ],
   secret: process.env.NEXTAUTH_SECRET,
-  adapter: MongoDBAdapter(clientPromise),
+  adapter: PrismaAdapter(prisma),
 });
