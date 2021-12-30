1c0552f307c7dd5600835e3875cc627840dcca17
EricWendel <eawendel19@gmail.com>
2021-12-30 00:45:24 -0600

    Single global prisma connection

--- pages/api/postRecipe.js
+++ pages/api/postRecipe.js
@@ -1,11 +1,10 @@
 import { getSession } from "next-auth/react";
-import { PrismaClient } from "@prisma/client";
+import { prisma } from "../../db/index.ts";
 
 export default async function handler(req, res) {
   const session = await getSession({ req });
   if (req.method === "POST" && session) {
     const { title, user, email, image, desc } = req.body;
-    const prisma = new PrismaClient();
     const result = await prisma.recipe.create({
       data: {
         title: title,
